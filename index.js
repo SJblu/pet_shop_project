85a7318e3b4b921f8d72ca044e02010fcd3a84d1
SILVIO SOUZA <sjblu7@gmail.com>
2021-01-17 21:25:39 -0400

    Adicionando arquivos aula 01

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => console.log('Server rodando na porta 3000'))
+app.get('/', (req, res) => res.send('Servidor Rodando!! '))
